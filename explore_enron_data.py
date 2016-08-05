165fde3d92663f47b5aa038de796fc3fea300260
Tegan Lohman <tmlohman@gmail.com>
2016-08-05 11:02:04 -0700

    revert master to previous corrected version

--- explore_enron_data.py
+++ explore_enron_data.py
@@ -14,30 +14,18 @@
     enron_data["SKILLING JEFFREY K"]["bonus"] = 5600000
     
 """
-import sys
-import os
+
 import pickle
-sys.path.append("tools/")
-from helper_functions import get_outliers
 import numpy as np
 from collections import defaultdict
 
-
-
 enron_data = pickle.load(open("final_project_dataset.pkl", "r"))
-
 
 # print out possible features to explore
 feature_list = []
 for k in enron_data.values()[0]:
 	feature_list.append(k)
-	#print k
-	
-#outliers = get_outliers(feature_list, enron_data)
-#count = 0
-#for k,v in outliers.iteritems():
-	#count += v
-#print count
+	print k
 
 
 # get count of persons of interest and total persons
@@ -60,8 +48,7 @@ def make_dict(data, feature):
 		if data[p][feature] != "NaN" and p != "TOTAL":
 			new_dict[p] = data[p][feature]
 	return new_dict
-
-			
+		
 # get stats on a given feature
 def get_stats(data):
 	min_val = min(data)
@@ -73,76 +60,38 @@ def get_stats(data):
 		if d > (mean + 2*std) or d < (mean - 2*std):
 			outliers.append(d)
 	return min_val, max_val, std, mean, outliers
-
-# get outliers for desired features	
-def get_outliers(feature_list, data):
-	outliers = defaultdict(int)
-	for f in feature_list:
-		if f != "poi":
-			try:
-				f_data = make_dict(data, f)
-				print f
-				print f + " outliers:"
-				stats = get_stats(f_data.values())
-				for outlier in stats[-1]:
-					for name, value in f_data.iteritems():
-						if value == outlier and name in poi_list:
-							print name + ": "+ str(value) + " (poi)"
-							outliers[name] += 1
-						if value == outlier and name not in poi_list:
-							print name + ": "+ str(value)
-							outliers[name] += 1
-				for name in enron_data:
-					if name not in outliers.keys():
-						outliers[name] = 0
-				print " "
-			except:
-				print "feature " + f + " not numerical"  + "\n"
-
-
-	return outliers
 	
-## get stats and outliers for desired features	
-#def get_outliers(feature_list, enron_data):
-	#outliers = defaultdict(int)
-	#all_stats = {}
-	#for f in feature_list:
-		#if f != "poi":
-			#try:
-				#f_data = make_dict(enron_data, f)
-				#print f
-				#stats = get_stats(f_data.values())
-				#all_stats[f] = stats
-				#print f + " outliers:"
-				#for outlier in stats[-1]:
-					#for name, value in f_data.iteritems():
-						#if value == outlier and name in poi_list:
-							#print name + ": "+ str(value) + " (poi)"
-							#outliers[name] += 1
-						#if value == outlier:
-							#print name + ": "+ str(value)
-							#outliers[name] += 1
-				#for name in enron_data:
-					#if name not in outliers.keys():
-						#outliers[name] = 0
-				#print " "
-			#except:
-				#print "feature " + f + " not numerical"  + "\n"
-	#return outliers, all_stats
-		
-outliers = get_outliers(feature_list, enron_data)	
+# get stats and outliers for desired features	
+features = []
+outliers = defaultdict(int)
 
-	
+
+for f in feature_list:
+	if f != "poi":
+		try:
+			f_data = make_dict(enron_data, f)
+			print f
+			stats = get_stats(f_data.values())
+			print stats
+			print f + " outliers:"
+			for outlier in stats[-1]:
+				for name, value in f_data.iteritems():
+					if value == outlier and name in poi_list:
+						print name + ": "+ str(value) + " (poi)"
+						outliers[name] += 1
+					if value == outlier and name not in poi_list:
+						print name + ": "+ str(value)
+						outliers[name] += 1
+			print " "
+		except:
+			print "feature " + f + " not numerical"  + "\n"
+
+count = 0		
 for key, value in outliers.iteritems():
 	if key in poi_list:
 		print key + ": " + str(value) + " (poi)"
 	else:
 		print key + ": " + str(value)
-		
-count = 0
-for value in outliers.values():
-	count += value
-print count	
-
-#
-
+	count += value	
+	
+print count
